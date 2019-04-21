767f545cf25f06464dbb18257735fe63d3086e0e
mixmix <whimful@gmail.com>
2019-04-21 17:56:00 +1200

    minimal ssb integration

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const { ApolloServer, gql } = require('apollo-server')
+const Client = require('ssb-client')
+const Config = require('ssb-config/inject')
 
-const typeDefs = gql`
-  type Query {
-    "a simple string to get started!"
-    hello: String
-  }
-`
+const config = Config('ssb', {})
 
-const resolvers = {
-  Query: {
-    hello: () => 'hello worlds!'
-  }
-}
+Client(config.keys, config, (err, ssb) => {
+  if (err) throw err
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
+  const typeDefs = gql`
+    type Query {
+      "a simple string to get started!"
+      hello: String,
+      whoami: String
+    }
+  `
 
-server.listen()
-  .then(({ url }) => {
-    console.log(`Server ready at ${url}`)
+  const context = (req) => ({
+    ssb: ssb
   })
+
+  const resolvers = {
+    Query: {
+      hello: () => 'hello worlds!',
+      whoami: (_, __, context) => {
+        return new Promise((resolve, reject) => {
+          context.ssb.whoami((err, data) => {
+            if (err) return reject(err)
+            resolve(data.id)
+          })
+        })
+      }
+    }
+  }
+
+  const apollo = new ApolloServer({
+    typeDefs,
+    context,
+    resolvers
+  })
+
+  apollo.listen()
+    .then(({ url }) => {
+      console.log(`Server ready at ${url}`)
+    })
+})
